ef1261ac42a1ee4ad9d3f4c69921f80b930dd2f9
ryanmdavis <ryan.m.davis5@gmail.com>
2015-05-12 16:42:46 -0400

    added readme.m

---
+++ readme.m
@@ -0,0 +1,6 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This file was downloaded from:
+%       https://github.com/ryanmdavis/GE-MSE-HOT
+%
+% Ryan M Davis.             rmd12@duke.edu                       05/12/2015
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%end%header
